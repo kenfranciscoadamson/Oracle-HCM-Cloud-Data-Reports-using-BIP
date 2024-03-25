4299a0ee335acae2868fcc9e2884404f925d8202
James Kennith Francisco <jameskennithgfrancisco@gmail.com>
2024-03-25 14:24:11 +0800

    Update Locations with Attribute10.sql

--- Locations with Attribute10.sql
+++ Locations with Attribute10.sql
@@ -1,16 +1,10 @@
-SELECT DISTINCT PLDF.ATTRIBUTE10 "Attribute10" --sample reference flexfield from the client
+-- The main purpose of this SQL for data reports is to extract Location Codes for a Location in reference to a flexfield attribute (Attribute10 in this instance), with filter to quick search for said flexfield attribute, and additional filters for a distinct Set S0033 (in addition, COMMON). Also, added Location names since there could be several Location Codes per flexfield attribute.
+
+SELECT DISTINCT PLDF.ATTRIBUTE10 "Attribute10" -- sample reference point from the client
 	, PL.INTERNAL_LOCATION_CODE "LocationCode"
-	, FSS.SET_NAME "SetName"
+	, PLDFT.LOCATION_NAME "Location"
+	, FSS.SET_NAME "BusinessUnit" -- for our client, Set Names are similarly Business Unit names
 	, FSS.SET_CODE "SetCode"
-	, PLDFT.LOCATION_NAME "LocationName"
-	, TO_CHAR(PLDF.EFFECTIVE_START_DATE, 'YYYY/MM/DD') "EffectiveStartDate"
-	, TO_CHAR(PLDF.EFFECTIVE_END_DATE, 'YYYY/MM/DD') "EffectiveEndDate"
-	, HIKM.SOURCE_SYSTEM_OWNER "SourceSystemOwner"
-	, HIKM.SOURCE_SYSTEM_ID "SourceSystemId"
-	, PL.CREATED_BY "CreatedBy"
-	, TO_CHAR(PL.CREATION_DATE, 'YYYY/MM/DD') "CreationDate"
-	, PL.LAST_UPDATED_BY "LastUpdatedBy"
-	, TO_CHAR(PL.LAST_UPDATE_DATE, 'YYYY/MM/DD') "LastUpdateDate"
 FROM PER_LOCATIONS PL
 	, PER_LOCATION_DETAILS_F PLDF
 	, PER_LOCATION_DETAILS_F_TL PLDFT
@@ -20,15 +14,12 @@ WHERE 1 = 1
 	AND PL.LOCATION_ID = PLDF.LOCATION_ID
 	AND PLDF.LOCATION_DETAILS_ID = PLDFT.LOCATION_DETAILS_ID
 	AND PL.SET_ID = FSS.SET_ID
+	AND SYSDATE BETWEEN PLDF.EFFECTIVE_START_DATE AND PLDF.EFFECTIVE_END_DATE
+	AND SYSDATE BETWEEN PLDFT.EFFECTIVE_START_DATE AND PLDFT.EFFECTIVE_END_DATE
 	AND FSS.LANGUAGE = FSS.SOURCE_LANG
 	AND PLDFT.LANGUAGE = PLDFT.SOURCE_LANG
 	AND PL.LOCATION_ID = HIKM.SURROGATE_ID
 	AND HIKM.OBJECT_NAME = 'Location'
-	AND INTERNAL_LOCATION_CODE = nvl(:LocationCode, PL.INTERNAL_LOCATION_CODE)
-	AND SET_NAME = nvl(:SetName, FSS.SET_NAME)
-	AND LOCATION_NAME LIKE nvl(:LocationName, PLDFT.LOCATION_NAME)
-	AND DECODE(:SelectRecords, 'ALL', SYSDATE, PLDFT.EFFECTIVE_START_DATE) <= SYSDATE
-	AND DECODE(:SelectRecords, 'ALL', SYSDATE, PLDFT.EFFECTIVE_END_DATE) >= SYSDATE
-	AND DECODE(:SelectRecords, 'ALL', SYSDATE, PLDF.EFFECTIVE_START_DATE) <= SYSDATE
-	AND DECODE(:SelectRecords, 'ALL', SYSDATE, PLDF.EFFECTIVE_END_DATE) >= SYSDATE
-ORDER BY PL.INTERNAL_LOCATION_CODE, TO_CHAR(PLDF.EFFECTIVE_START_DATE, 'YYYY/MM/DD')
+	AND ATTRIBUTE10 = nvl(:StandortID, PLDF.ATTRIBUTE10)
+	AND FSS.SET_CODE IN ('S0033', 'COMMON')
+ORDER BY PLDF.ATTRIBUTE10, FSS.SET_NAME, PL.INTERNAL_LOCATION_CODE
